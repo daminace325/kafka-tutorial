66b3c3a9143f51a66f24e902978bd313ebe73eaa
daminace325 <daminkhan325@gmail.com>
2025-10-21 22:28:10 +0530

    Implement Kafka consumer in analytic service to process payment success messages

---
+++ services/analytic-service/index.js
@@ -0,0 +1,35 @@
+// 34:11
+import { Kafka } from "kafkajs"
+
+const kafka = new Kafka({
+    clientId: "analytic-service",
+    brokers: ["localhost:9094"]
+});
+
+const consumer = kafka.consumer({ groupId: "analytic-service" });
+
+const run = async () => {
+    try {
+        await consumer.connect();
+        await consumer.subscribe({
+            topic: "payment-successful",
+            fromBeginning: true
+        });
+
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                const value = message.value.toString();
+                const { userId, cart } = JSON.parse(value);
+                const total = cart.reduce((acc, item) => acc + item.price, 0);
+
+                console.log(`Analytic consumer: User ${userId} paid ${total}`);
+                
+            }
+        });
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+run();
